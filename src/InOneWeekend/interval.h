ec47876256ad2690fcc71776b858da449532ea69
Josh5778 <joshvalastro2005@gmail.com>
2025-09-27 23:32:42 +1000

    up to chapter 12.1

--- src/InOneWeekend/interval.h
+++ src/InOneWeekend/interval.h
@@ -2,33 +2,28 @@
 #define INTERVAL_H
 
 class interval {
-    public:
-        double min, max;
-        
-        interval() : min(+infinity), max(-infinity) {} // default interval is empty
-        interval(double min, double max) : min(min), max(max) {}
+ public:
+  double min, max;
 
-        double size() const {return max - min; }
+  interval() : min(+infinity), max(-infinity) {}  // default interval is empty
+  interval(double min, double max) : min(min), max(max) {}
 
-        bool contains(double x) const {
-            return min <= x && x <= max;
-        }
+  double size() const { return max - min; }
 
-        bool surrounds(double x) const {
-            return min < x && x < max;
-        }
+  bool contains(double x) const { return min <= x && x <= max; }
 
-        double clamp(double x) const {
-            if (x < min) return min;
-            if (x > max) return max;
-            return x;
-        }
+  bool surrounds(double x) const { return min < x && x < max; }
 
-        static const interval empty, universe;
+  double clamp(double x) const {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+  }
+
+  static const interval empty, universe;
 };
 
 const interval interval::empty = interval(+infinity, -infinity);
 const interval interval::universe = interval(-infinity, +infinity);
-
 
 #endif
