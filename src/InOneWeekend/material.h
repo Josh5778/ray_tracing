f5ef84fe256f85429447e462c78a1124c7bdb64c
Josh5778 <joshvalastro2005@gmail.com>
2025-09-28 20:39:24 +1000

    the_next_week-chapter3

--- src/InOneWeekend/material.h
+++ src/InOneWeekend/material.h
@@ -23,7 +23,8 @@ class lambertian : public material {
 
     // catch degenerate scatter direction
     if (scatter_direction.near_zero()) scatter_direction = rec.normal;
-    scattered = ray(rec.p, scatter_direction);
+
+    scattered = ray(rec.p, scatter_direction, r_in.time());
     attenuation = albedo;
     return true;
   }
@@ -41,7 +42,7 @@ class metal : public material {
                ray& scattered) const override {
     vec3 reflected = reflect(r_in.direction(), rec.normal);
     reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
-    scattered = ray(rec.p, reflected);
+    scattered = ray(rec.p, reflected, r_in.time());
     attenuation = albedo;
     return dot(scattered.direction(), rec.normal) > 0;
   }
@@ -73,7 +74,7 @@ class dielectric : public material {
       direction = refract(unit_direction, rec.normal, ri);
     }
 
-    scattered = ray(rec.p, direction);
+    scattered = ray(rec.p, direction, r_in.time());
     return true;
   }
 
